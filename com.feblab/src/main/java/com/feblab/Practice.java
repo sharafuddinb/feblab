eeddd1842f3583388f808a0ea7b416caf11f595b
sharafuddinb <sharafuddin.baryali@gmail.com>
2020-07-15 20:07:20 -0400

    this is feblab commit

---
+++ com.feblab/src/main/java/com/feblab/Practice.java
@@ -0,0 +1,12 @@
+package com.feblab;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("I am printing");
+
+	}
+
+}
