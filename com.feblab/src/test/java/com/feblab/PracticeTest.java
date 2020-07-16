fb31f996f4eeed099f8bebb2b8b13234f085b366
sharafuddinb <sharafuddin.baryali@gmail.com>
2020-07-15 20:12:43 -0400

    Code updated

---
+++ com.feblab/src/test/java/com/feblab/PracticeTest.java
@@ -0,0 +1,14 @@
+package com.feblab;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PracticeTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
